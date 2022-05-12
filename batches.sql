53ab833cb0da1b23e015986f60b26a963c57a0de
kronayne <kpr6aj@virginia.edu>
2022-05-12 15:47:39 -0400

    pivot tables and visuals

--- batches.sql
+++ batches.sql
@@ -1,20 +1,20 @@
 
 /*batch 1 606-3815 */
 select * from dbo.fact_table as facts
-WHERE facts.facts_key <=3815 /*1/5 of the total number of rows */
+WHERE facts.facts_key <=3815;
 
 /*batch 2 3816-7025*/
 select * from dbo.fact_table as facts
-WHERE facts.facts_key BETWEEN 3816 AND 7025
+WHERE facts.facts_key BETWEEN 3816 AND 7025;
 
 /*batch 3 7026-10234*/
 select * from dbo.fact_table as facts
-WHERE facts.facts_key BETWEEN 7026 AND 10234
+WHERE facts.facts_key BETWEEN 7026 AND 10234;
 
 /*batch 4 10235-13444*/
 select * from dbo.fact_table as facts
-WHERE facts.facts_key BETWEEN 10235 AND 13444
+WHERE facts.facts_key BETWEEN 10235 AND 13444;
 
 /*batch 5 13445-16650*/
 select * from dbo.fact_table as facts
-WHERE facts.facts_key >13444
+WHERE facts.facts_key >13444;
